8fd0a83297e0be994d264b550d03b7e751b3e703
rock204 <75961408+rock204@users.noreply.github.com>
2023-09-11 09:06:26 +0900

    監視ループと関数の配置

--- main.ts
+++ main.ts
@@ -3,14 +3,25 @@ input.onButtonPressed(Button.A, function () {
     mode = 1
     max = 0
 })
+function stop_display () {
+	
+}
 input.onButtonPressed(Button.B, function () {
     music.play(music.tonePlayable(262, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
     mode = 0
 })
+function sokutei () {
+	
+}
 let max = 0
 let mode = 0
 serial.writeLine("スタート")
 mode = 999
 basic.forever(function () {
-	
+    if (mode == 1) {
+        sokutei()
+    }
+    if (mode == 0) {
+        stop_display()
+    }
 })
