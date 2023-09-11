20f068a941ec585f8a34425374be01a6df49a58c
rock204 <75961408+rock204@users.noreply.github.com>
2023-09-11 09:00:42 +0900

    初期設定・ABボタン設定

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    mode = 1
+    max = 0
+})
+input.onButtonPressed(Button.B, function () {
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+    mode = 0
+})
+let max = 0
+let mode = 0
+serial.writeLine("スタート")
+mode = 999
 basic.forever(function () {
 	
 })
